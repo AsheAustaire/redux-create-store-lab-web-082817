7ad3953b96afa2d465573e2e7cfef23f1d8a83eb
Ashe Austaire <AsheAustaire@gmail.com>
2017-11-17 00:23:31 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,27 @@
-export default function createStore(reducer) {
-  // add your code here
+export default function createStore(reducer) { //Stores accept reducers as arguments
+
+  //state
+    let state;
+
+  //action
+    function dispatch(action) {
+      state = reducer(state, action);
+      render();
+    }
+
+    function getState() {
+      return state;
+    }
+
+    dispatch({type: '@@INIT'})
+
+  //new state
+  return {
+    dispatch: dispatch,
+    getState: getState
+  }
 }
+
 
 function render() {
   const container = document.getElementById('container');
